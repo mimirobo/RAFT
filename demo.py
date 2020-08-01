8921698bddfa137f8bef79b915791e3862b4a38e
mimirobo <m13.esmaeili@gmail.com>
2020-08-01 12:04:02 +0430

    batch demo

--- demo.py
+++ demo.py
@@ -36,7 +36,7 @@ def load_image_list(image_files):
     return padder.pad(images)[0]
         
 
-def viz(img, flo):
+def viz(img, flo , dir_name):
     global counter
     img = img[0].permute(1,2,0).cpu().numpy()
     flo = flo[0].permute(1,2,0).cpu().numpy()
@@ -46,13 +46,14 @@ def viz(img, flo):
     img_flo = np.concatenate([img, flo], axis=0)
 
     #cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
-    cv2.imwrite('saved_frames/img-{}.png'.format(counter), flo)
-    print("Optical FLow {} saved.".format(counter))
+    cv2.imwrite(dir_name+'/img-{}.png'.format(counter), flo)
+    print("Optical FLow {} saved in {}".format(counter, dir_name))
     counter = counter + 1
     #cv2.waitKey()
 
 
 def demo(args):
+    global counter
     model = torch.nn.DataParallel(RAFT(args))
     model.load_state_dict(torch.load(args.model))
 
@@ -60,26 +61,37 @@ def demo(args):
     model.to(DEVICE)
     model.eval()
 
-    with torch.no_grad():
-        images = glob.glob(os.path.join(args.path, '*.png')) + \
-                 glob.glob(os.path.join(args.path, '*.jpg')) + \
-                 glob.glob(os.path.join(args.path, '*.tif'))
+    parent_dir = args.input_dir + "/*"
+    all_sub_dirs = glob(parent_dir)
+    #Create Export Sub-Dirs
+    for sub_dir_path in all_sub_dirs:
+        output_dir_path = args.output_dir + "/" + os.path.basename(sub_dir_path)
+        os.mkdir(output_dir_path)
+        counter = 0
+        with torch.no_grad():
+            images = glob.glob(os.path.join(sub_dir_path, '*.png')) + \
+                    glob.glob(os.path.join(sub_dir_path, '*.jpg')) + \
+                    glob.glob(os.path.join(sub_dir_path, '*.tif'))
 
-        images = load_image_list(images)
-        for i in range(images.shape[0]-1):
-            image1 = images[i,None]
-            image2 = images[i+1,None]
+            images = load_image_list(images)
+            for i in range(images.shape[0]-1):
+                image1 = images[i,None]
+                image2 = images[i+1,None]
 
-            flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-            viz(image1, flow_up)
+                flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
+                viz(image1, flow_up, output_dir_path)
+        print("Sub-Dir {} done".format(os.path.basename(sub_dir_path)))
+    print("Completed")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help="restore checkpoint")
-    parser.add_argument('--path', help="dataset for evaluation")
+    #parser.add_argument('--path', help="dataset for evaluation")
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
+    parser.add_argument('--input_dir', help="input parent directory for groups of sub-dirs")
+    parser.add_argument('--output_dir', help="output parent directory for optical flow results")
     args = parser.parse_args()
 
     demo(args)
